7fa2b6f203c6a23c7ea0ef2da851515fe0c57dea
wkn816 <wkn816@gmail.com>
2021-04-25 20:03:32 +0900

    変数

--- mani.java
+++ mani.java
@@ -18,5 +18,19 @@ class Main {
     // 変数nameを出力してください
     System.out.println(name);
 
+    int number1 = 3;
+
+    // int型の変数number2を定義し、7を代入してください
+    int number2 = 7;
+
+    // number1 * number2を出力してください
+    System.out.println(number1 * number2);
+
+    // 変数textに「プログラミングを勉強しよう」を代入してください
+    String text = "プログラミングを勉強しよう";
+
+    // 「Progateで」と変数textを連結して出力してください
+    System.out.println("Progateで" + text);
+
   }
 }
