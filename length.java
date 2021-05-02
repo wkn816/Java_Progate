68ed75c190f2ea994afc5ba3326b83f9d2c3d619
wkn816 <wkn816@gmail.com>
2021-05-03 00:05:16 +0900

    add-拡張for文

--- length.java
+++ length.java
@@ -1,11 +1,22 @@
+// class Main {
+//   public static void main(String[] args) {
+//     String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+
+//     // for文を用いて、「私の名前は◯◯です」と出力してください
+//     for(int i = 0; i < names.length; i++){
+//       System.out.println("私の名前は" + names[i] + "です");
+//     }
+
+//   }
+// }
+
 class Main {
   public static void main(String[] args) {
     String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
 
-    // for文を用いて、「私の名前は◯◯です」と出力してください
-    for(int i = 0; i < names.length; i++){
-      System.out.println("私の名前は" + names[i] + "です");
+    // 配列用のfor文を用いて、「私の名前は◯◯です」と出力してください
+    for(String name : names){
+    System.out.println("私の名前は" + name + "です");
     }
-
   }
 }
