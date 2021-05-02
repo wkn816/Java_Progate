f3d23ef366b3560bbe238a7f99e4d52ea081124f
wkn816 <wkn816@gmail.com>
2021-05-02 23:56:35 +0900

    fix-ループ回数変更

--- for.java
+++ for.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     // for文を用いて、繰り返し処理をつくってください
-    for(int i = 1; i<=10; i++){
+    for(int i = 1; i<=15; i++){
       System.out.println(i + "回目のループです");
     }
   }
