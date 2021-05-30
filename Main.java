fd9d8472f1ac67cadb1bc87310fb622d0e9ff482
wkn816 <wkn816@gmail.com>
2021-05-30 23:59:59 +0900

    3-10Mainクラスで他クラスを呼び出す

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
-    Person.hello();
+    // printDataメソッドとfullNameメソッドの呼び出しを書き換えてください
+    Person.printData(Person.fullName("Kate", "Jones"), 27, 1.6, 50.0);
+    Person.printData(Person.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
   }
+
+  // 以下のメソッドすべてを、Personクラスに移動してください
 }
