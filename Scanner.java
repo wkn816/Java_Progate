763a11afa3d5341d2182977bcb9682acce58507e
wkn816 <wkn816@gmail.com>
2021-06-26 00:17:06 +0900

    scanner条件分岐

--- Scanner.java
+++ Scanner.java
@@ -8,6 +8,14 @@ class Main {
     String firstName = scanner.next();
     System.out.print("名字：");
     String lastName = scanner.next();
+    System.out.print("年齢：");
+    int age = scanner.nextInt();
     System.out.println("名前は" + firstName + " "+ lastName + "です");
+    System.out.println("年齢は" + age + "歳です");
+    if ( age>=20 ){
+      System.out.println("成年者です");
+    } else {
+      System.out.println("未成年者です");
+    }
   }
 }
