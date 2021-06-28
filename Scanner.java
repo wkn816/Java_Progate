264c746656847967f5ff7aa56f531dfa30841b84
wkn816 <wkn816@gmail.com>
2021-06-28 14:40:50 +0900

    道場コース-メソッドにまとめる

--- Scanner.java
+++ Scanner.java
@@ -6,16 +6,30 @@ class Main {
 
     System.out.print("名前：");
     String firstName = scanner.next();
+
     System.out.print("名字：");
     String lastName = scanner.next();
+
     System.out.print("年齢：");
     int age = scanner.nextInt();
+
+    System.out.print("身長(m)：：");
+    double heigh = scanner.nextDouble();
+
+    System.out.print("体重(kg)：");
+    double stmac = scanner.nextDouble();
+
     System.out.println("名前は" + firstName + " "+ lastName + "です");
+
     System.out.println("年齢は" + age + "歳です");
     if ( age>=20 ){
       System.out.println("成年者です");
     } else {
       System.out.println("未成年者です");
     }
+
+    System.out.println("身長は" + heigh + "mです");
+    System.out.println("体重は" + stmac + "kgです");
+
   }
 }
