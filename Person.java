a49f12ccbea13c15fb3da930de71154150c5c6d4
wkn816 <wkn816@gmail.com>
2021-07-02 20:29:40 +0900

    add-BMIで健康状態の判定

--- Person.java
+++ Person.java
@@ -1,21 +1,26 @@
 class Person {
-
-  public static void printData(String firstName, String lastName, int age, double heigh, double stmac){
-
-    System.out.println("名前は" + firstName + " "+ lastName + "です");
+  public static void printData(String firstName, String lastName, int age, double height, double weight) {
+    System.out.println("名前は" + fullName(firstName, lastName) + "です");
 
     System.out.println("年齢は" + age + "歳です");
-    if ( age>=20 ){
-      System.out.println("成年者です");
-    } else {
-      System.out.println("未成年者です");
-    }
+      if (age >= 20) {
+        System.out.println("成年者です");
+      } else {
+        System.out.println("未成年者です");
+      }
 
-    System.out.println("身長は" + heigh + "mです");
-    System.out.println("体重は" + stmac + "kgです");
+    System.out.println("身長は" + height + "mです");
+
+    System.out.println("体重は" + weight + "kgです");
 
     double bmi = bmi(height, weight);
     System.out.println("BMIは" + Math.round(bmi) + "です");
+
+    if(isHealthy(bmi)){
+      System.out.println("健康です");
+    }else{
+      System.out.println("健康ではありません");
+    }
   }
 
   public static String fullName(String firstName, String lastName) {
@@ -25,4 +30,8 @@ class Person {
   public static double bmi(double height, double weight) {
     return weight / height / height;
   }
-}
+
+  public static boolean isHealthy(double bmi) {
+    return bmi >=18.5 && bmi <25.0;
+  }
+  }
