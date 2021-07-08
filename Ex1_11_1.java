74e3a1d08e84ab7337eba912869645d3dc5496cb
wkn816 <wkn816@gmail.com>
2021-07-08 23:59:59 +0900

    while文の繰り返し処理

--- Ex1_11_1.java
+++ Ex1_11_1.java
@@ -5,15 +5,21 @@ while文を使用して、6の目がでるまでサイコロを降り続ける�
 ----------------------------------------------------*/
 class Ex1_11_1 {
 	public static void main (String[] args) {
+
+    int diceNumber = 0;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		while( diceNumber != 6){
+			diceNumber =1 + (int)(Math.random() * 6.0);
+			System.out.println(diceNumber);
+		}
+		System.out.println("6が出たのでを終了します");
+
+
+
+
+
+
+
+
 	}
 }
